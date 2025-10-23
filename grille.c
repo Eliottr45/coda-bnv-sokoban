13cf88298d91e9f85a0b96776dfd007bf204d424
Tourtelier Eliot <eliot.tourtelier@gmail.com>
2025-10-23 14:20:58 +0200

    création de la fonction extract grid qui permet d'extraire une map d'un fichier txt

--- grille.c
+++ grille.c
@@ -1,3 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "function.h"
+
+char * extract_grid(int id_map) // selection de la map à l'aide d'un scanf dans main précédent l'appel de la fonction
+{
+    // ouverture de mon fichier map.txt
+    FILE *fp = NULL; // déclaration en dehors des if
+
+    if(id_map == 1)
+    {
+        FILE *fp = fopen("map1.txt", "r");
+
+        if (fp == NULL)
+        {
+            printf("Le fichier map1.txt n'a pas pu être ouvert\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    else if(id_map == 2)
+    {
+        FILE *fp = fopen("map2.txt", "r");
+
+        if (fp == NULL)
+        {
+            printf("Le fichier map2.txt n'a pas pu être ouvert\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    else if(id_map == 3)
+    {
+        FILE *fp = fopen("map3.txt", "r");
+
+        if (fp == NULL)
+        {
+            printf("Le fichier map3.txt n'a pas pu être ouvert\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    else
+    {
+        printf("Le numéro de map que vous avez choisi n'existe pas.\n");
+        exit(0);
+    }
+
+    // récupération des données du map.txt
+    char * content;
+    char buffer[10];
+
+    content = malloc(sizeof(char));
+    content[0] = '\0';
+
+    while(fgets(buffer, 10, fp) != NULL)
+    {
+        content = realloc(content, (strlen(buffer) * sizeof(char)) + strlen(content) * sizeof(char));
+        content = strcat(content, buffer);
+    }
+    fclose(fp);
+    return content;
+}
